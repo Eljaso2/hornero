f7046a34250fdc1f583aacd0a602b57621c1ea84
Eljaso <alejandro.jasinski@gmail.com>
2026-07-31 16:22:53 -0300

    Bump SW v315 + ?ver=304

--- projects/hornero/app/service-worker.js
+++ projects/hornero/app/service-worker.js
@@ -4,7 +4,7 @@
 // This means: push changes → open app → see changes immediately, no cache clearing
 // Production: revert to stale-while-revalidate and remove { cache: 'no-cache' }
 
-var CACHE_NAME = 'hornero-v314';
+var CACHE_NAME = 'hornero-v315';
 var ASSETS = [
   './css/hornero.css',
   './js/db.js',
